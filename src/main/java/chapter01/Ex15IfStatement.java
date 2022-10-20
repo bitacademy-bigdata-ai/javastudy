6e2e37390ca986535b5e980b952d2e24ebf02ee3
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-20 16:28:47 +0900

    제어문 - 조건문 #1

---
+++ src/main/java/chapter01/Ex15IfStatement.java
@@ -0,0 +1,17 @@
+package chapter01;
+
+public class Ex15IfStatement {
+
+	public static void main(String[] args) {
+		// Q2: 최대값을 판별하는 조건을 완성해 보세요.
+		int a1 = 20;
+		int a2 = 10;
+		int a3 = 50;
+
+		
+
+
+		// 최대값은 50입니다.
+	}
+
+}
