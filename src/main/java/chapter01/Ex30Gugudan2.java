ece677fb17fc9b01c53e8d4f0b789bb3395f3c3f
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-21 13:48:40 +0900

    제어문 #2

---
+++ src/main/java/chapter01/Ex30Gugudan2.java
@@ -0,0 +1,49 @@
+package chapter01;
+
+public class Ex30Gugudan2 {
+
+	public static void main(String[] args) {
+		//System.out.println("1 X 1 = 1");
+		//System.out.println("1 X 1 = 1");
+		
+		System.out.print("1 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("2 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("3 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("4 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("5 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("6 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("7 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("8 X 1 = 1");
+		System.out.print("\t");
+		System.out.print("9 X 1 = 1");
+		
+		System.out.print("\n");
+		
+		System.out.print("1 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("2 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("3 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("4 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("5 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("6 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("7 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("8 X 2 = 1");
+		System.out.print("\t");
+		System.out.print("9 X 2 = 1");		
+
+	}
+
+}
