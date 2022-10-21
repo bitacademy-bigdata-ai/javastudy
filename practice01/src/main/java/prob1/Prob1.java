a98d2d240cc84e1f1f9f8c9a9448f44e1934ba68
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-21 15:36:55 +0900

    practice01

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Prob1 {
 	public static void main(String[] args) {
+		System.out.print("수를 입력하시오 : ");
 		Scanner scanner = new Scanner( System.in );
 		
 		/* 코드 작성 */
