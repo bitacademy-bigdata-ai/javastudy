42836ab073ffbf418842487fc4a9b583e141c0c6
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-24 11:36:14 +0900

    practice02 #0

---
+++ practice02/src/main/java/prob03/Prob03.java
@@ -0,0 +1,21 @@
+package prob03;
+
+public class Prob03 {
+	public static void main(String args[]) {
+		char[] charArray = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
+
+		// 원래 배열 원소 출력
+		printCharArray(charArray);
+
+		// 공백 문자 바꾸기
+		replaceSpace(charArray);
+
+		// 수정된 배열 원소 출력
+		printCharArray(charArray);
+	}
+	
+	public static void printCharArray(char[] cs) {
+		System.out.println(cs);
+	}
+
+}
---
+++ practice02/src/main/java/prob04/Prob04.java
@@ -0,0 +1,22 @@
+package prob04;
+public class Prob04 {
+
+	public static void main(String[] args) {
+		char[] c1 = reverse("Hello World");
+		printCharArray(c1);
+		
+		char[] c2 = reverse("Java Programming!");
+		printCharArray(c2);
+	}
+	
+	public static char[] reverse(String str) {
+		char[] cs = str.toCharArray();
+		
+		
+		return null;
+	}
+
+	public static void printCharArray(char[] array){
+		System.out.println(array);
+	}
+}
