6d61fcc97aa34cb9b440aae9fd1bbfa91f1d2a9d
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-25 14:01:04 +0900

    객체지향프로그래밍 #2

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,11 +1,43 @@
 package chapter03;
 
 public class Goods {
-	String name;
-	int price;
-	int countStock;
-	int countSold;
+	private String name;
+	private int price;
+	private int countStock;
+	private int countSold;
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCountStock() {
+		return countStock;
+	}
+
+	public void setCountStock(int countStock) {
+		this.countStock = countStock;
+	}
+
+	public int getCountSold() {
+		return countSold;
+	}
+
+	public void setCountSold(int countSold) {
+		this.countSold = countSold;
+	}
+
 	public String toString() {
 		return "Goods [name=" + name + ", price=" + price + ", countStock=" + countStock + ", countSold=" + countSold
 				+ "]";
