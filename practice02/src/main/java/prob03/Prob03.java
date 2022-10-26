5f81ea86071b05aab43e42fbe2c0f7f89eb0db56
bitacademy-bigdata-ai <115518907+bitacademy-bigdata-ai@users.noreply.github.com>
2022-10-26 16:54:53 +0900

    Update Prob03.java

--- practice02/src/main/java/prob03/Prob03.java
+++ practice02/src/main/java/prob03/Prob03.java
@@ -2,20 +2,27 @@ package prob03;
 
 public class Prob03 {
 	public static void main(String args[]) {
-		char[] charArray = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
+		char c[] = { 'T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 'p', 'e', 'n', 'c', 'i', 'l', '.' };
 
 		// 원래 배열 원소 출력
-		printCharArray(charArray);
+		printCharArray(c);
 
 		// 공백 문자 바꾸기
-		replaceSpace(charArray);
+		replaceSpace(c);
 
 		// 수정된 배열 원소 출력
-		printCharArray(charArray);
-	}
-	
-	public static void printCharArray(char[] cs) {
-		System.out.println(cs);
+		printCharArray(c);
 	}
 
+	public static void replaceSpace(char[] c) {
+		for(int i = 0; i < c.length; i++) {
+			if(c[i] == ' ') {
+				c[i] = ',';
+			}
+		}
+	}
+
+	public static void printCharArray(char[] c) {
+		System.out.println(c);
+	}
 }
