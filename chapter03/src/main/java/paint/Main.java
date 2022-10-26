58244bf25f4f8fb9f7e974349d8c03718fca7811
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-26 15:26:11 +0900

    다형성 - 오버라이딩

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -3,7 +3,6 @@ package paint;
 public class Main {
 
 	public static void main(String[] args) {
-		
 		Point pt1 = new Point(10, 50);
 		//pt1.setX(10);
 		//pt1.setY(50);
@@ -12,19 +11,21 @@ public class Main {
 		//pt1.disappear();
 		pt1.show(false);
 		
-		ColorPoint pt2 = new ColorPoint();
+		Point pt2 = new ColorPoint();
 		pt2.setX(100);
 		pt2.setY(200);
-		pt2.setColor("red");
+		((ColorPoint)pt2).setColor("red");
 		
-		drawColorPoint(pt2);
+		//drawColorPoint(pt2);
+		drawPoint(pt2);
+		pt2.show(false);
 	}
 	
 	public static void drawPoint(Point pt) {
 		pt.show();
 	}
 	
-	public static void drawColorPoint(ColorPoint pt) {
-		pt.show();
-	}
+	// public static void drawColorPoint(ColorPoint pt) {
+	//	pt.show();
+	//}
 }
