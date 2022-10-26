4ed57e6c220d130d9504a94832de6afcdd3d2026
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-26 14:55:16 +0900

    상속

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -4,17 +4,27 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Point pt = new Point(10, 50);
-		//pt.setX(10);
-		//pt.setY(50);
+		Point pt1 = new Point(10, 50);
+		//pt1.setX(10);
+		//pt1.setY(50);
 		
-		drawPoint(pt);
+		drawPoint(pt1);
+		//pt1.disappear();
+		pt1.show(false);
 		
-		//pt.disappear();
-		pt.show(false);
+		ColorPoint pt2 = new ColorPoint();
+		pt2.setX(100);
+		pt2.setY(200);
+		pt2.setColor("red");
+		
+		drawColorPoint(pt2);
 	}
 	
 	public static void drawPoint(Point pt) {
+		pt.show();
+	}
+	
+	public static void drawColorPoint(ColorPoint pt) {
 		pt.show();
 	}
 }
