b235697b7fd1688a1e1d514e0c64bbdfe5fa3e55
bitacademy-bigdata-ai <115518907+bitacademy-bigdata-ai@users.noreply.github.com>
2022-10-26 16:52:13 +0900

    Update Prob4.java

--- practice01/src/main/java/prob4/Prob4.java
+++ practice01/src/main/java/prob4/Prob4.java
@@ -10,11 +10,15 @@ public class Prob4 {
 		System.out.print("문자열을 입력하세요 : ");
 		String text = scanner.nextLine();
 
-		for(int i = 0; i < text.length(); i++) {
-			char c = text.charAt(i);
-			System.out.println(c);
+		int length = text.length();
+		for(int i = 0; i < length; i++) {
+			for(int j = 0; j <= i; j++) {
+				System.out.print(text.charAt(j));
+			}
+
+			System.out.println("");
 		}
-		
+
 		scanner.close();
 	}
 
