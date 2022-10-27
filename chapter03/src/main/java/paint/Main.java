9048cf8594cda080956860780a7ceddb9c5c6be5
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-27 10:59:26 +0900

    개체지향프로그래밍 - #5

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -6,21 +6,37 @@ public class Main {
 		Point pt1 = new Point(10, 50);
 		//pt1.setX(10);
 		//pt1.setY(50);
-		
 		drawPoint(pt1);
 		//pt1.disappear();
 		pt1.show(false);
 		
-		Point pt2 = new ColorPoint();
-		pt2.setX(100);
-		pt2.setY(200);
-		((ColorPoint)pt2).setColor("red");
-		
+		Point pt2 = new ColorPoint(100, 200, "red");
+		// pt2.setX(100);
+		// pt2.setY(200);
+		// ((ColorPoint)pt2).setColor("red");
 		//drawColorPoint(pt2);
 		drawPoint(pt2);
 		pt2.show(false);
+		
+		Triangle triangle = new Triangle();
+		// drawTriangle(triangle);
+		drawShape(triangle);
+		
+		Rectangle rectangle = new Rectangle();
+		drawShape(rectangle);
+		
+		Circle circle = new Circle();
+		drawShape(circle);
+	}
+
+	public static void draw(IDrawable drawble) {
+		
 	}
 	
+	public static void drawShape(Shape shape) {
+		shape.draw();
+	}
+
 	public static void drawPoint(Point pt) {
 		pt.show();
 	}
@@ -28,4 +44,5 @@ public class Main {
 	// public static void drawColorPoint(ColorPoint pt) {
 	//	pt.show();
 	//}
+
 }
