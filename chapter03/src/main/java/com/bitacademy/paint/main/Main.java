db8cc6859da1c0f544304cc7a4b4f1ef338312eb
bitacademy.bigdata.ai <bitacademy.bigdata.ai@gmail.com>
2022-10-27 14:19:05 +0900

    packaging

--- chapter03/src/main/java/com/bitacademy/paint/main/Main.java
+++ chapter03/src/main/java/com/bitacademy/paint/main/Main.java
@@ -1,8 +1,18 @@
-package paint;
+package com.bitacademy.paint.main;
+
+import com.bitacademy.paint.i.Drawable;
+import com.bitacademy.paint.point.ColorPoint;
+import com.bitacademy.paint.point.Point;
+import com.bitacademy.paint.shape.Circle;
+import com.bitacademy.paint.shape.Rectangle;
+import com.bitacademy.paint.shape.Shape;
+import com.bitacademy.paint.shape.Triangle;
+import com.bitacademy.paint.text.GraphicText;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
 		Point pt1 = new Point(10, 50);
 		//pt1.setX(10);
 		//pt1.setY(50);
@@ -35,6 +45,17 @@ public class Main {
 		
 		GraphicText gt = new GraphicText("Hello World");
 		draw(gt);
+		
+		// instanceof test
+		System.out.println(circle instanceof Object);
+		System.out.println(circle instanceof Shape);
+		System.out.println(circle instanceof Circle);
+		// 오류: class는 hierachy 상위와 하위만 instanceof 연산자를 사용할 수 있다.
+		// System.out.println(circle instanceof Rectangle);
+
+		// 하지만, interface는 hierachy와 상관없이 instanceof 연산자를 사용할 수 있다.
+		System.out.println(circle instanceof Drawable);
+		System.out.println(circle instanceof Runnable);
 	}
 
 	public static void draw(Drawable drawable) {
